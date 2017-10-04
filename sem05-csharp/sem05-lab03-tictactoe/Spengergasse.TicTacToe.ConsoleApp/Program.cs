b62b74e0e6984cd8ee3594f976251b49a6afde65
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-10-04 13:49:49 +0200

    Add new labs

---
+++ sem05-csharp/sem05-lab03-tictactoe/Spengergasse.TicTacToe.ConsoleApp/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Spengergasse.TicTacToe.Library;
+
+namespace Spengergasse.TicTacToe.ConsoleApp {
+  public class Program {
+    private static Game game;
+    public static void Main(string[] args) {
+      while (true) {
+        game = new Game();
+        Play();
+        Console.Write("Play again? [y/N] ");
+        if (Console.ReadKey(true).KeyChar.ToString().ToUpper() != "Y") {
+          Environment.Exit(0);
+        }
+      }
+    }
+
+    static void Play() {
+      int? winner = null;
+
+      while (winner == null) {
+        Console.Clear();
+        Move();
+        winner = game.CheckWinner();
+      }
+
+      Console.Clear();
+      DrawGrid();
+
+      Console.WriteLine();
+
+      if (winner == 0) {
+        Console.WriteLine("It's a tie!");
+      } else {
+        Console.WriteLine("{0} won!", GetPlayer(winner));
+      }    
+    }
+
+    static string GetPlayer(int? player) {
+      if (player == 1) return "X";
+      if (player == 2) return "O";
+      return null;
+    }
+
+    static void Move() {
+      var cells = game.Cells;
+      DrawGrid();
+
+      Console.WriteLine();
+      Console.WriteLine("It's {0}'s turn.", GetPlayer(game.CurrentPlayer));
+
+      Console.Write("Please enter a valid number between 0 and 8: ");
+      var input = GetNumber(0, 8);
+      Console.WriteLine(input);
+
+      game.Set(input);
+    }
+
+    static void DrawGrid() {
+      Console.WriteLine();
+      var i = 0;
+      var cells = game.Cells;
+      while (i < cells.Length) {
+        var col = i % 3;
+        if (col == 0) Console.Write("  ");
+        Console.Write(cells[i] == 0 ? i.ToString() : GetPlayer(cells[i]));
+        Console.Write(col == 2 ? '\n' : ' ');
+
+        i++;
+      }
+    }
+
+    static int GetNumber(int min, int max) {
+      try {
+        var input = int.Parse(Console.ReadKey(true).KeyChar.ToString());
+        return input >= min && input <= max
+          ? input
+          : GetNumber(min, max);
+      } catch (FormatException) {
+        return GetNumber(min, max);
+      }
+    }
+  }
+}
---
+++ sem05-csharp/sem05-lab03-tictactoe/Spengergasse.TicTacToe.Library/Game.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+
+namespace Spengergasse.TicTacToe.Library {
+  public class Game {
+    public int[] Cells { get; } = new int[9];
+
+    public int CurrentPlayer { get; private set; } = 1;
+
+    public void Set(int cell) {
+      if (Cells[cell] == 0) {
+        Cells[cell] = CurrentPlayer;
+        IteratePlayer();
+      }
+    }
+
+    private void IteratePlayer() =>
+      CurrentPlayer = CurrentPlayer == 1 ? 2 : 1;
+
+    public int? CheckWinner() {
+      if (CheckFull()) return 0;
+      if (CheckWinner(1)) return 1;
+      if (CheckWinner(2)) return 2;
+      return null;
+    }
+
+    public bool CheckFull() {
+      foreach (int cell in Cells) {
+        if (cell == 0) return false;
+      }
+      return true;
+    }
+
+    public bool CheckWinner(int player) {
+      if (Cells[0] == player && Cells[4] == player && Cells[8] == player) {
+        return true;
+      }
+      if (Cells[2] == player && Cells[4] == player && Cells[6] == player) {
+        return true;
+      }
+      for (int offset = 0; offset < 3; offset++) {
+        if (Cells[0 + 3 * offset] == player
+          && Cells[1 + 3 * offset] == player
+          && Cells[2 + 3 * offset] == player) {
+          return true;
+        }
+        if (Cells[0 + offset] == player
+          && Cells[3 + offset] == player
+          && Cells[6 + offset] == player) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    public override string ToString() => string.Join("", Cells) + " Current Player: " + CurrentPlayer;
+  }
+}
