d11219953339b7e8f3ce87be8911d6a68e7c8b71
Samuel Kaiser <samuel.kaiser01@gmail.com>
2018-01-23 11:00:42 +0100

    Initial commit

---
+++ sem05-the05-git/GitTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Elisha, you around?");
+    }
+  }
+}
