b6fcec35de25e5d2f1af93f2efaf004c1e813577
Samuel Kaiser <samuel.kaiser01@gmail.com>
2018-02-14 15:17:06 +0100

    Add WPF DB example

---
+++ sem05-csharp/sem05-the06-wpf-db/Spengergasse.WpfDb1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spengergasse.WpfDb1 {
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window {
+    Schule2000Entities db = new Schule2000Entities();
+
+    public MainWindow() {
+      InitializeComponent();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs args) {
+      ClassesList.ItemsSource = db.klassens.ToList();
+    }
+
+    private void SaveButton_Click(object sender, RoutedEventArgs e) {
+      db.SaveChanges();
+    }
+  }
+}
