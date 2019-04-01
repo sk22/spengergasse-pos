4c6f335e27f3a09416f8d6237aeac9c40c972f1d
Samuel Kaiser <samuel.kaiser01@gmail.com>
2019-04-01 14:46:49 +0200

    Init REST API

---
+++ sem07-android/lab10-rest/wassersport/public/index.php
@@ -0,0 +1,25 @@
+<?php
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
+$app = new \Slim\App();
+
+$app->get('/', function ($request, $response, $args) {
+    return 'oida wos';
+});
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->write("Hello " . $args['name']);
+});
+
+$app->run();
