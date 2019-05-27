e6ea3c803d0571d417b9531f784782cecb2459eb
Samuel Kaiser <samuel.kaiser01@gmail.com>
2019-05-27 10:10:15 +0200

    Add lab12

---
+++ sem07-android/lab12-reponsive-ui/app/src/main/java/at/spengergasse/kai17521/lab12/TextFragment.java
@@ -0,0 +1,16 @@
+package at.spengergasse.kai17521.lab12;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TextFragment extends Fragment {
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.text_fragment, container, false);
+  }
+}
